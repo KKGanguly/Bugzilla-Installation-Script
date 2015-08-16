2b02e4a29cbf2545069df09e3e17af804b227ba7
Kishan Kumar Ganguly <kkganguly.iit.du@gmail.com>
2015-08-16 18:15:59 +0600

    Update bugzillascript.sh

--- bugzillascript.sh
+++ bugzillascript.sh
@@ -1,7 +1,7 @@
 sudo apt-get install apache2 mysql-server-5.6 libappconfig-perl libdate-calc-perl libtemplate-perl libmime-perl build-essential libdatetime-timezone-perl libdatetime-perl libemail-sender-perl libemail-mime-perl libemail-mime-modifier-perl libdbi-perl libdbd-mysql-perl libcgi-pm-perl libmath-random-isaac-perl libmath-random-isaac-xs-perl apache2-mpm-prefork libapache2-mod-perl2 libapache2-mod-perl2-dev libchart-perl libxml-perl libxml-twig-perl perlmagick libgd-graph-perl libtemplate-plugin-gd-perl libsoap-lite-perl libhtml-scrubber-perl libjson-rpc-perl libdaemon-generic-perl libtheschwartz-perl libtest-taint-perl libauthen-radius-perl libfile-slurp-perl libencode-detect-perl libmodule-build-perl libnet-ldap-perl libauthen-sasl-perl libtemplate-perl-doc libfile-mimeinfo-perl libhtml-formattext-withlinks-perl libgd-dev lynx-cur python-sphinx
-wget https://ftp.mozilla.org/pub/mozilla.org/webtools/bugzilla-4.4.6.tar.gz
-tar -xvzf bugzilla-4.4.6.tar.gz
-sudo mv bugzilla-4.4.6 /var/www/html/bugzilla
+wget https://ftp.mozilla.org/pub/mozilla.org/webtools/bugzilla-5.0.tar.gz
+tar -xvzf bugzilla-5.0.tar.gz
+sudo mv bugzilla-5.0 /var/www/html/bugzilla
 sudo chown -R www-data:www-data /var/www/html/bugzilla
 mysql -u root -p < bug.sql
 sudo cp -p /etc/mysql/my.cnf /etc/mysql/my.cnf.orig
